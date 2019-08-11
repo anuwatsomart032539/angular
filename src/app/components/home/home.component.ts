98f4b6ba9535e62615e111e909239036c44717f1
anuwat somart <anuwatsomart@hotmail.com>
2019-08-11 12:21:11 +0700

    first commit

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../../models/product'
+
+import { ProductService } from 'src/app/services/product.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  products: Product[]
+
+  constructor(private productService:ProductService) { }
+
+  ngOnInit() {
+    this.productService.getAllProducts().subscribe((res:Product[]) => {
+      this.products = res
+      console.log(this.products)
+    })
+  }
+
+  deleteProduct(id) {
+    this.productService.deleteProduct(id).subscribe(res =>{
+      this.ngOnInit()
+    })
+  }
+}
